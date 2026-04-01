dfa900e76b4c86fba3b1a726a5d8e003ee62f7aa
Kim-Lan <me@kim-lan.com>
2026-03-31 18:09:42 -0700

    Custom categories

---
+++ cats.js
@@ -0,0 +1,76 @@
+const cats = {};
+
+// 1. d20 fantasy high characters
+cats["d20 fantasy high characters"] = ["gorgug", "ayda", "fig", "fabian", "kristen", "adaine", "aelwyn", "riz", "sklonda", "sandra lynn", "gilear", "jawbone", "tracker", "penny", "ostentatia", "zelda", "biz", "katja", "cathilda", "porter", "gorthalax", "boggy", "ragh", "zayn", "basrar"];
+
+// 2. parts of a cell
+cats["parts of a cell"] = ["centriole", "ribosome", "mitochondria", "cytoplasm", "golgi apparatus", "chloroplast", "lysosome", "endoplasmic reticulum", "nucleolus", "cell membrane", "glycosome", "microtubule", "vacuole", "peroxisome", "nucleus", "nucleosome", "flagellum", "plasmid", "cilium", "melanosome", "myofibril", "ocelloid", "cnidocyst", "autophagosome", "stress granule"];
+
+// 3. stardew villagers' favorite gifts
+cats["stardew villagers' favorite gifts"] = ["beer", "lobster", "coffee", "pale ale", "pepper poppers", "parsnip soup", "pizza", "ice cream", "emerald", "green tea", "sandfish", "roots platter", "frog egg", "frozen tear", "wine", "sashimi", "void egg", "amethyst", "ruby", "pomegranate", "pufferfish", "spicy eel", "chocolate cake", "fish taco", "pancakes"];
+
+// 4. birds
+cats["birds"] = ["spotted towhee", "common loon", "snowy egret", "great horned owl", "chickadee", "cardinal", "golden-crowned sparrow", "trumpeter swan", "greater sage-grouse", "roseate spoonbill", "acorn woodpecker", "amazon kingfisher", "american bittern", "atlantic puffin", "barn swallow", "bell's vireo", "brant", "bobolink", "bushtit", "canvasback", "killdeer", "osprey", "pine siskin", "purple martin", "say's phoebe"];
+
+// 5. board and card games
+cats["board and card games"] = ["love letter", "betrayal at house on the hill", "mysterium", "coup", "avalon", "one night ultimate werewolf", "settlers of catan", "moose master", "flip 7", "azul", "magic the gathering", "disney lorcana", "muffin time", "chess", "sorry!", "monopoly", "unstable unicorns", "pandemic", "exploding kittens", "everdell", "hues and cues", "wingspan", "wavelength", "clue", "trouble"];
+
+// 6. stardew forageables
+cats["stardew forageables"] = ["horseradish", "daffodil", "dandelion", "leek", "holly", "crystal fruit", "snow yam", "winter root", "blackberry", "salmonberry", "wild plum", "hazelnut", "common mushroom", "fiddlehead fern", "purple mushroom", "morel", "chanterelle", "sweet pea", "spring onion", "spice berry", "crocus", "coconut", "magma cap", "ginger", "cave carrot"];
+
+// 7. stardew crops
+cats["stardew crops"] = ["summer squash", "carrot", "corn", "blue jazz", "kale", "potato", "rhubarb", "red cabbage", "poppy", "starfruit", "tomato", "sunflower", "amaranth", "broccoli", "cranberries", "pineapple", "sweet gem berry", "ancient fruit", "taro root", "powdermelon", "qi fruit", "pumpkin", "summer spangle", "coffee bean", "garlic"];
+
+// 8. pokemon
+cats["pokemon"] = ["iron treads", "scream tail", "type: null", "wattrel", "calyrex", "meditite", "rellor", "espathra", "houndstone", "great tusk", "minior", "tapu fini", "wooloo", "eiscue", "drakloak", "ursaluna", "fezandipiti", "iron crown", "seel", "zarude", "walking wake", "wo-chien", "golem", "klang", "tyrunt"];
+
+// 9. minecraft blocks
+cats["minecraft blocks"] = ["diorite", "calcite", "sandstone", "netherite", "spruce log", "jack o'lantern", "sculk catalyst", "diamond", "iron", "gold", "quartz", "coarse dirt", "gravel", "mushroom stem", "crafting table", "mycelium", "stone", "granite", "andesite", "deepslate", "hay bale", "nylium", "blue ice", "pale moss", "terracotta"];
+
+// 10. chemical elements
+cats["chemical elements"] = ["prometheum", "gallium", "germanium", "strontium", "cobalt", "nickel", "hydrogen", "oxygen", "radon", "arsenic", "tin", "bromine", "krypton", "molybdenum", "potassium", "calcium", "tungsten", "praseodymium", "silver", "bismuth", "lead", "mercury", "fluorine", "berkelium", "gadolinium"];
+
+// 11. genshin impact characters
+cats["genshin impact characters"] = ["ifa", "neuvillette", "emilie", "lynette", "flins", "ororon", "ayaka", "chasca", "baizhu", "durin", "diluc", "amber", "klee", "jean", "columbina", "nahida", "qiqi", "aino", "bennett", "candace", "yoimiya", "xilonen", "wanderer", "razor", "gaming"];
+
+// 12. honkai star rail characters
+cats["honkai star rail characters"] = ["asta", "mydei", "phainon", "aglaea", "acheron", "archer", "saber", "boothill", "blade", "firefly", "kafka", "himeko", "sparkle", "topaz", "aventurine", "hysilens", "guinaifen", "hook", "serval", "march 7th", "misha", "black swan", "evernight", "castorice", "fugue"];
+
+// 13. Greek mythology
+cats["Greek mythology"] = ["clytemnestra", "pandora", "epimetheus", "aeetes", "circe", "medea", "jason", "europa", "io", "daphne", "adonis", "asterius", "hector", "paris", "helen", "achilles", "patroclus", "odysseus", "calypso", "arachne", "icarus", "daedalus", "minos", "polyphemus", "aeolus"];
+
+// 14. hades boons
+cats["hades boons"] = ["support fire", "boiling point", "heartbreak flourish", "heaven's vengeance", "different league", "empty inside", "huge catch", "blood frenzy", "vicious cycle", "exit wounds", "hide breaker", "pressure points", "hunter's mark", "holy shield", "bronze skin", "last stand", "rare crop", "snow burst", "winter harvest", "after party", "positive outlook", "bad influence", "peer pressure", "side hustle", "ocean's bounty"];
+
+// 15. modern literature
+cats["modern literature"] = ["this is how you lose the time war", "katabasis", "the hate u give", "last night at the telegraph club", "tomorrow and tomorrow and tomorrow", "the seven husbands of evelyn hugo", "the hunger games", "a court of thorns and roses", "a deadly education", "the starless sea", "the night circus", "the chosen and the beautiful", "the song of achilles", "severance", "atmosphere", "the ministry of time", "clap when you land", "aristotle and dante discover the secrets of the universe", "mexican gothic", "the jasmine throne", "chemistry", "stone blind", "crying in h mart", "the gentleman's guide to vice and virtue", "on earth we're briefly gorgeous"];
+
+// 16. high school musical songs
+cats["high school musical songs"] = ["gotta go my own way", "start of something new", "stick to the status quo", "bop to the top", "breaking free", "what I've been looking for", "get'cha head in the game", "when there was me and you", "we're all in this together", "fabulous", "work this out", "you are the music in me", "what time is it", "i don't dance", "bet on it", "everyday", "all for one", "i want it all", "right here, right now", "can i have this dance", "just wanna be with you", "the boys are back", "a night to remember", "walk away", "scream"];
+
+// 17. stardew song titles
+cats["stardew song titles"] = ["cloud country", "grandpa's theme", "settling in", "it's a big world outside", "the valley comes alive", "wild horseradish jam", "flower dance", "distant banjo", "pelican town", "a glimpse of the other world", "nature's crescendo", "the sun can bend an orange sky", "tropicala", "ghost synth", "the smell of mushroom", "raven's descent", "nocturne of ice", "the wind can be still", "ancient", "a golden star is born", "calico desert", "buttercup melody", "a flicker in the deep", "visitor to the unknown", "in the deep woods"];
+
+// 18. porter and madeon
+cats["porter and madeon"] = ["shelter", "pay no mind", "get your wish", "look at the sky", "mirror", "something comforting", "knock yourself out", "cheerleader", "perfect pinterest garden", "trying to feel alive", "hi!", "the prince", "miracle", "technicolor", "isometric", "imperium", "nonsense", "beings", "blossom", "goodbye to a world", "sad machine", "musician", "divinity", "language", "unfold"];
+
+// 19. rhetorical devices
+cats["rhetorical devices"] = ["synecdoche", "asyndeton", "polysyndeton", "zeugma", "simile", "metaphor", "anaphora", "chiasmus", "allusion", "hyperbole", "litotes", "analepsis", "antiphrasis", "epistrophe", "metonymy", "onomatopoeia", "oxymoron", "syllepsis", "metanoia", "syllogism", "aphorism", "analogy", "personification", "euphemism", "apostrophe"];
+
+// 20. math terms
+cats["math terms"] = ["fraction", "sine", "cosine", "secant", "tangent", "exponent", "integral", "derivative", "differential", "slope", "linear", "parabola", "sphere", "hyperbola", "ellipse", "circle", "surface area", "intercept", "asymptote", "proof", "radius", "perimeter", "circumference", "volume", "dimension"];
+
+// 21. stardew speedruns
+cats["stardew speedruns"] = ["crafts room", "pantry", "fish tank", "boiler room", "vault", "community center", "joja", "joja movie theater", "marriage", "drink mayo", "boat", "the missing bundle", "mines level 50", "true perfection", "4 candles", "child", "junimo kart", "hat mouse", "talk to everyone", "sea urchin", "underwearsoup", "milk & cookies", "hidden note statues", "posh", "skull caverns 100"];
+
+// 22. shakespearen characters
+cats["shakespearen characters"] = ["ophelia", "mercutio", "benvolio", "romeo", "juliet", "petruchio", "desdemona", "iago", "shylock", "macduff", "tybalt", "falstaff", "banquo", "claudius", "polonius", "laertes", "miranda", "gertrude", "horatio", "othello", "hermia", "helena", "titania", "goneril", "cordelia"];
+
+// 23. arrows in action and the home team songs
+cats["arrows in action and the home team songs"] = ["only be mine", "permanent vacation", "cheekbones", "on", "uncomfortably numb", "high", "loud", "compromise", "endeavor", "all the ways i could die", "entropy", "hell", "danger", "the credits", "worthy", "honey", "hello sunlight!", "honest", "feel it again", "celebration", "brag", "stay awake", "overtime", "wide eyes", "the grey"];
+
+// 24. pokemon gym leaders
+cats["pokemon gym leaders"] = ["candice", "maylene", "roark", "byron", "crasher wake", "gardenia", "volkner", "falkner", "norman", "blaine", "misty", "jasmine", "clair", "roxanne", "winona", "fantina", "burgh", "clay", "iris", "nessa", "kabu", "iono", "raihan", "lenora", "chili"];
+
+// 25. music terms
+cats["music terms"] = ["rubato", "coda", "allegro", "largo", "rallentando", "caesura", "crescendo", "staccato", "forte", "piano", "andante", "accent", "cadenza", "cantabile", "dolce", "fermata", "sforzando", "grave", "octave", "scherzo", "sharp", "flat", "syncopation", "tempo", "tenuto"];
